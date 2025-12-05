cf65288a34af65584302f440e6a967f373a13d6c
Divyansh Garg <HIMYNAMEISDIVYANSH@GMAIL.COM>
2025-12-05 17:57:38 +0530

    Update Part2.cpp

--- Day2/Part2.cpp
+++ Day2/Part2.cpp
@@ -4,50 +4,34 @@ using namespace std;
 bool isInvalid(long long num) {
     string s = to_string(num);
     int n = s.length();
-
-    // Try all possible block sizes
     for (int block = 1; block <= n / 2; block++) {
-
-        // Only if string length is a multiple of block size
         if (n % block != 0) continue;
-
         string part = s.substr(0, block);
-
         bool ok = true;
-        // Check if whole string is repeated 'part'
         for (int i = 0; i < n; i++) {
             if (s[i] != part[i % block]) {
                 ok = false;
                 break;
             }
         }
-
-        if (ok) return true; // invalid
+        if (ok) return true; 
     }
-
-    return false; // not invalid
+    return false; 
 }
 
 int main() {
     string input;
     getline(cin, input);
-
     long long total = 0;
-
     long long L = 0, R = 0;
     bool readingLeft = true;
-
     for (int i = 0; i <= input.size(); i++) {
-
         if (i == input.size() || input[i] == ',') {
-
-            // check all numbers in the range
             for (long long x = L; x <= R; x++) {
                 if (isInvalid(x)) {
                     total += x;
                 }
             }
-
             L = R = 0;
             readingLeft = true;
         }
@@ -59,7 +43,6 @@ int main() {
             else R = R * 10 + (input[i] - '0');
         }
     }
-
     cout << total;
     return 0;
 }
